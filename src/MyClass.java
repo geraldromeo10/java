8debc9521e031b1328f5c363e10ecb026ddcb5b8
geraldromeo11 <gerald.borromeo10@gmail.com>
2020-02-05 15:09:08 -0600

    looped commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,5 +2,12 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
         System.out.println("Here is my second commit");
+
+        int[] numbers;
+        numbers = new int[4];
+
+        for (int i = 0; i < numbers.length; ++i) {
+            System.out.println(i);
+        }
     }
 }
